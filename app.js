db3ec216ab0d88f83df94bc50e61a6e411108451
devdeepkumar <139380154+devdeepkumar@users.noreply.github.com>
2024-02-27 13:27:08 +0530

    app.js

--- app.js
+++ app.js
@@ -19,22 +19,6 @@ function addTask() {
   inputText.value = "";
 }
 
-// addbtn.addEventListener("click", function () {
-//   if (inputText.value === "") {
-//     alert("You need to write Somthing");
-//   } else {
-//     let li = document.createElement("li");
-//     li.innerHTML = inputText.value;
-//     taskList.appendChild(li);
-
-//     let remv = document.createElement("button");
-//     remv.innerHTML = "remove";
-//     remv.classList.add("remove");
-//     li.appendChild(remv);
-//   }
-//   inputText.value = "";
-// });
-
 let ol = document.querySelector("ul");
 
 ol.addEventListener("click", (event) => {
